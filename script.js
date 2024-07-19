1d9896abdcba74d67f8b02e47baea03162d2c323
aidar <a1dargti@gmail.com>
2024-07-19 08:56:13 +0500

    add hover effect

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
-  let grid = 16;
+  let grid = 64;
   const wrapper = document.querySelector(".wrapper");
   for (let i = 0; i < grid; i++) {
     const col = document.createElement("div");
     wrapper.appendChild(col);
     col.classList.add("grid");
+
     for (let j = 0; j < grid; j++) {
       const row = document.createElement("div");
       col.appendChild(row);
       row.classList.add("gridItem");
+      row.addEventListener("mouseenter", function () {
+        row.classList.add("gridItemActive");
+      });
     }
   }
 });
