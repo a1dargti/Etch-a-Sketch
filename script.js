134ead807ac63e3f6429010fbefe9155a39de2e6
aidar <a1dargti@gmail.com>
2024-07-20 11:18:06 +0500

    added button

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-document.addEventListener("DOMContentLoaded", function () {
-  let grid = 64;
+let grid = 16;
+let btn = document.querySelector("button");
+let test = document.querySelector("#test");
+const container = document.querySelector(".wrapper");
+btn.addEventListener("click", function () {
+  let grid = prompt("Введите число");
+  if (grid <= 100 && grid > 2) {
+    gridfn(grid);
+  } else {
+    alert("Please enter number between 2 and 100");
+  }
+
+  console.log(grid);
+});
+function gridfn(grid) {
+  container.innerHTML = "";
   const wrapper = document.querySelector(".wrapper");
   for (let i = 0; i < grid; i++) {
     const col = document.createElement("div");
@@ -15,4 +29,6 @@ document.addEventListener("DOMContentLoaded", function () {
       });
     }
   }
-});
+}
+
+gridfn(grid);
